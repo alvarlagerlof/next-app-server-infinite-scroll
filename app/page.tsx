30116cf25db3d13b2f7fc4ffe0b3c7007a77a6e3
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-11-09 07:02:18 +0100

    initial broken

--- app/page.tsx
+++ app/page.tsx
@@ -1,57 +1,43 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export default function Home() {
+  // import { useVirtualizer } from "@tanstack/react-virtual";
+
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js 13!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://beta.nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js 13</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Explore the Next.js 13 playground.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates/next.js/app-directory?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
+        <h1>List!</h1>
+        {/** @ts-ignore */}
+        <List length={20} />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
+}
+
+type Item = {
+  albumId: number;
+  id: number;
+  title: string;
+  url: string;
+  thumbnailUrl: string;
+};
+
+async function List({ length }: { length: number }) {
+  const response = await fetch(
+    `http://jsonplaceholder.typicode.com/photos?_start=${0}&_limit=${le}`
+  );
+  const people: Item[] = await response.json();
+
+  return (
+    <ul>
+      {people.map((item) => {
+        return (
+          <li key={item.id}>
+            <h2>{item.title}</h2>
+            <p>{item.url}</p>
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
