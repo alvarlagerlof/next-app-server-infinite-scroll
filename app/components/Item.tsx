b5f6d2529f25036ae027e7e131a6432c9ba140db
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2022-12-11 23:04:20 +0100

    Fix ts

--- app/components/Item.tsx
+++ app/components/Item.tsx
@@ -28,6 +28,7 @@ export function Item({ title, id, thumbnailUrl }: ItemData) {
 export const ItemLoading = forwardRef(function ItemLoading(_, ref) {
   return (
     <div
+      // @ts-ignore
       ref={ref}
       className="bg-neutral-100 dark:bg-neutral-800 border border-neutral-200  dark:border-neutral-700 rounded-lg px-4 py-6"
     >
