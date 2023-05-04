2e651f5ad3f8a817fa74ab015026ceb1705d4c73
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2023-05-05 01:47:29 +0200

    cleanup

--- app/components/Code.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export function Code({ children }: { children: ReactNode }) {
-  return (
-    <pre className="inline bg-neutral-100 dark:bg-neutral-800 border border-neutral-200 dark:border-neutral-700 rounded px-1 py-0.5">
-      {children}
-    </pre>
-  );
-}
