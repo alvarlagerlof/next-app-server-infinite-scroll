3496b816b359f0bd99c56f61d64dfc7a0625dfac
Steve McClain <steve@marmalade.co>
2023-07-04 10:42:26 -0400

    Remove unused config settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-    scrollRestoration: true,
-    runtime: "experimental-edge",
-  },
   images: {
     remotePatterns: [
       {
